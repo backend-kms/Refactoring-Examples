1575b61e51151be51378b4afc4b70e98f7522504
backend-kms <mindu6424@naver.com>
2024-12-18 17:29:57 +0900

    fix: performance switch문 함수 분리

--- performances/performance.js
+++ performances/performance.js
@@ -4,6 +4,29 @@ const fs = require('fs');
 const plays = JSON.parse(fs.readFileSync('plays.json', 'utf8'));
 const invoices = JSON.parse(fs.readFileSync('invoices.json', 'utf8'));
 
+function amountFor(perf, play) {
+    let thisAmount = 0;
+
+    switch (play.type) {
+        case "tragedy":
+            thisAmount = 40000;
+            if (perf.audience > 30) {
+                thisAmount += 1000 * (perf.audience - 30);
+            }
+            break;
+        case "comedy":
+            thisAmount = 30000;
+            if (perf.audience > 20) {
+                thisAmount += 10000 + 500 * (perf.audience - 20);
+            }
+            thisAmount += 300 * perf.audience;
+            break;
+        default:
+            throw new Error(`unknown type: ${play.type}`);
+    }
+    return thisAmount;
+}
+
 // Statement 생성 함수
 function statement(invoice, plays) {
     let totalAmount = 0;
@@ -17,25 +40,7 @@ function statement(invoice, plays) {
 
     for (let perf of invoice.performances) {
         const play = plays[perf.playID];
-        let thisAmount = 0;
-
-        switch (play.type) {
-            case "tragedy":
-                thisAmount = 40000;
-                if (perf.audience > 30) {
-                    thisAmount += 1000 * (perf.audience - 30);
-                }
-                break;
-            case "comedy":
-                thisAmount = 30000;
-                if (perf.audience > 20) {
-                    thisAmount += 10000 + 500 * (perf.audience - 20);
-                }
-                thisAmount += 300 * perf.audience;
-                break;
-            default:
-                throw new Error(`unknown type: ${play.type}`);
-        }
+        let thisAmount = amountFor(perf, play)
 
         // add volume credits
         volumeCredits += Math.max(perf.audience - 30, 0);
