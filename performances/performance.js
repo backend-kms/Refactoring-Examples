78c3d679b99011211341ff78bc9f53894ed1a58b
backend-kms <mindu6424@naver.com>
2024-12-19 11:29:28 +0900

    fix: Removing Total Volume Credits (내부함수 구조)

--- performances/performance.js
+++ performances/performance.js
@@ -4,65 +4,75 @@ const fs = require('fs');
 const plays = JSON.parse(fs.readFileSync('plays.json', 'utf8'));
 const invoices = JSON.parse(fs.readFileSync('invoices.json', 'utf8'));
 
-function playFor(aPerformance) {
-    return plays[aPerformance.playID]
-}
-
-function amountFor(aPerformance) {
-    let result = 0;
-
-    switch (playFor(aPerformance).type) {
-        case "tragedy":
-            result = 40000;
-            if (aPerformance.audience > 30) {
-                result += 1000 * (aPerformance.audience - 30);
-            }
-            break;
-        case "comedy":
-            result = 30000;
-            if (aPerformance.audience > 20) {
-                result += 10000 + 500 * (aPerformance.audience - 20);
-            }
-            result += 300 * aPerformance.audience;
-            break;
-        default:
-            throw new Error(`unknown type: ${playFor(aPerformance).type}`);
-    }
-    return result;
-}
-
-function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
-    return result;
-}
-
-function usd(aNumber) {
-    return new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD",
-        minimumFractionDigits: 2
-    }).format(aNumber / 100);
-}
-
 // Statement 생성 함수
 function statement(invoice, plays) {
-    let totalAmount = 0;
-    let volumeCredits = 0;
+
     let result = `Statement for ${invoice.customer}\n`;
-
     for (let perf of invoice.performances) {
-        volumeCredits += volumeCreditsFor(perf);
-
-        // print line for this order
-        result += ` ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
-        totalAmount += amountFor(perf);
+        result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
     }
 
-    result += `Amount owed is ${usd(totalAmount)}\n`;
-    result += `You earned ${volumeCredits} credits\n`;
+    result += `Amount owed is ${usd(totalAmount())}\n`;
+    result += `You earned ${totalVolumeCredits()} credits\n`;
     return result;
+
+    function playFor(aPerformance) {
+        return plays[aPerformance.playID]
+    }
+
+    function amountFor(aPerformance) {
+        let result = 0;
+
+        switch (playFor(aPerformance).type) {
+            case "tragedy":
+                result = 40000;
+                if (aPerformance.audience > 30) {
+                    result += 1000 * (aPerformance.audience - 30);
+                }
+                break;
+            case "comedy":
+                result = 30000;
+                if (aPerformance.audience > 20) {
+                    result += 10000 + 500 * (aPerformance.audience - 20);
+                }
+                result += 300 * aPerformance.audience;
+                break;
+            default:
+                throw new Error(`unknown type: ${playFor(aPerformance).type}`);
+        }
+        return result;
+    }
+
+    function volumeCreditsFor(aPerformance) {
+        let result = 0;
+        result += Math.max(aPerformance.audience - 30, 0);
+        if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
+        return result;
+    }
+
+    function usd(aNumber) {
+        return new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD",
+            minimumFractionDigits: 2
+        }).format(aNumber / 100);
+    }
+
+    function totalVolumeCredits() {
+        let result = 0;
+        for (let perf of invoice.performances) {
+            result += volumeCreditsFor(perf);
+        }
+        return result;
+    }
+
+    function totalAmount() {
+        let result = 0;
+        for (let perf of invoice.performances) {
+            result += amountFor(perf);
+        }
+        return result;
+    }
 }
 
 // 테스트: invoices 배열을 순회하여 모든 청구서 출력
