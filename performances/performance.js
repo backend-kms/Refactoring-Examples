0af315eb4a3a3011d75b5c55808d156ed76d10ec
backend-kms <mindu6424@naver.com>
2024-12-19 10:33:37 +0900

    fix: performance volumeCredits 함수로 추출

--- performances/performance.js
+++ performances/performance.js
@@ -31,6 +31,12 @@ function amountFor(aPerformance) {
     return result;
 }
 
+function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
+    if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
+    return result;
+}
 
 // Statement 생성 함수
 function statement(invoice, plays) {
@@ -44,11 +50,7 @@ function statement(invoice, plays) {
     }).format;
 
     for (let perf of invoice.performances) {
-        // add volume credits
-        volumeCredits += Math.max(perf.audience - 30, 0);
-
-        // add extra credit for every ten comedy attendees
-        if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+        volumeCredits += volumeCreditsFor(perf);
 
         // print line for this order
         result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
